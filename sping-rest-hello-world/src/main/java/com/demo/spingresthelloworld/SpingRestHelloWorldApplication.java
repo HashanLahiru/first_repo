216a30eba11719639b99a8e72c4f78b47a9978d0
HashanLahiru <97427288+HashanLahiru@users.noreply.github.com>
2022-01-13 08:49:17 +0530

    added hello world project

---
+++ sping-rest-hello-world/src/main/java/com/demo/spingresthelloworld/SpingRestHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.demo.spingresthelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingRestHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingRestHelloWorldApplication.class, args);
+	}
+
+}
---
+++ sping-rest-hello-world/src/test/java/com/demo/spingresthelloworld/SpingRestHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.spingresthelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingRestHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
